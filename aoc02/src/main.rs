f28cc9b90cfe7baefbecd0e6457a92363b47c173
Stephen Hines <srhines@gmail.com>
2024-12-03 20:40:40 -0800

    Problem 2 part 2 for AOC2024

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -57,10 +57,41 @@ fn compute_safe(rows: &Vec<Vec<u32>>) -> u32 {
     safe
 }
 
+fn compute_safe_with_problem_dampener(rows: &Vec<Vec<u32>>) -> u32 {
+    let mut safe = 0;
+    for row in rows {
+        if check_safety(row) == true {
+            safe += 1;
+        } else {
+            // Just clone a duplicate row and remove an item to try them
+            let len = row.len();
+            let mut i = 0;
+            let mut done = false;
+            while i < len && !done {
+                let mut new_row = row.clone();
+                new_row.remove(i);
+                if check_safety(&new_row) == true {
+                    safe += 1;
+                    done = true;
+                }
+                i += 1;
+            }
+        }
+    }
+    println!("safe (pd): {}", safe);
+    safe
+}
+
 #[test]
 fn test_prelim() {
     let safe = compute_safe(&parse_lines(&get_input("prelim.txt")));
     assert_eq!(safe, 2);
+}
+
+#[test]
+fn test_prelim2() {
+    let safe = compute_safe_with_problem_dampener(&parse_lines(&get_input("prelim.txt")));
+    assert_eq!(safe, 4);
 }
 
 #[test]
@@ -69,7 +100,15 @@ fn test_part1() {
     assert_eq!(safe, 463);
 }
 
+#[test]
+fn test_part2() {
+    let safe = compute_safe_with_problem_dampener(&mut parse_lines(&get_input("input.txt")));
+    assert_eq!(safe, 514);
+}
+
 fn main() {
     compute_safe(&parse_lines(&get_input("prelim.txt")));
     compute_safe(&parse_lines(&get_input("input.txt")));
+    compute_safe_with_problem_dampener(&parse_lines(&get_input("prelim.txt")));
+    compute_safe_with_problem_dampener(&parse_lines(&get_input("input.txt")));
 }
