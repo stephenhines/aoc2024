6aa1f482c1f25cd1009bbedbba432e0572969708
Stephen Hines <srhines@gmail.com>
2024-12-24 02:06:22 -0500

    Problem 23 part 2 for AOC2024
    Brute force but we'll see if I find time to write a better version.

--- aoc23/src/main.rs
+++ aoc23/src/main.rs
@@ -75,6 +75,67 @@ fn find_trios(map: &HashMap<String, Vec<String>>) -> usize {
     trios
 }
 
+fn get_combos(map: &HashMap<String, Vec<String>>, start: &String) -> Vec<Vec<String>> {
+    let mut combos = Vec::new();
+    let connections = map.get(start).unwrap();
+    let c_len = connections.len() as u32;
+
+    let total = 2u64.pow(c_len);
+    for i in 0..total {
+        let mut new_vec = Vec::new();
+        new_vec.push(start.clone());
+        for j in 0..c_len {
+            let p = 1 << j;
+            if i & p != 0 {
+                new_vec.push(connections[j as usize].clone());
+            }
+        }
+        new_vec.sort(); // Always sort to keep it easier to check what we've tried already
+        combos.push(new_vec);
+    }
+    combos
+}
+
+fn check_clique(map: &HashMap<String, Vec<String>>, clique: &Vec<String>) -> bool {
+    for m in clique {
+        let connections = map.get(m).unwrap();
+        for n in clique {
+            if m != n {
+                if !connections.contains(n) {
+                    return false;
+                }
+            }
+        }
+    }
+    true
+}
+
+fn find_cliques(map: &HashMap<String, Vec<String>>) -> String {
+    let mut tried: HashSet<Vec<String>> = HashSet::new();
+    let mut max_clique = Vec::new();
+    let mut max_clique_len = 0;
+    for key in map.keys() {
+        //println!("Trying key {}: {} {:?}", key, map.get(key).unwrap().len(), map.get(key).unwrap());
+        let try_vec = get_combos(map, key);
+        for clique in try_vec {
+            if !tried.contains(&clique) {
+                if clique.len() > max_clique_len {
+                    if check_clique(map, &clique) {
+                        max_clique = clique.clone();
+                        max_clique_len = max_clique.len();
+                        //dbg!(&clique);
+                    }
+                }
+                tried.insert(clique);
+            }
+        }
+    }
+    //dbg!(&max_clique);
+    let password = max_clique.join(",");
+    println!("Password: {password}");
+    password
+}
+
 #[test]
 fn test_prelim() {
     let trios = find_trios(&read_network_map(&get_input("prelim.txt")));
@@ -87,7 +148,21 @@ fn test_part1() {
     assert_eq!(trios, 1476);
 }
 
+#[test]
+fn test_prelim2() {
+    let password = find_cliques(&read_network_map(&get_input("prelim.txt")));
+    assert_eq!(password, "co,de,ka,ta");
+}
+
+#[test]
+fn test_part2() {
+    let password = find_cliques(&read_network_map(&get_input("input.txt")));
+    assert_eq!(password, "ca,dw,fo,if,ji,kg,ks,oe,ov,sb,ud,vr,xr");
+}
+
 fn main() {
     find_trios(&read_network_map(&get_input("prelim.txt")));
     find_trios(&read_network_map(&get_input("input.txt")));
+    find_cliques(&read_network_map(&get_input("prelim.txt")));
+    find_cliques(&read_network_map(&get_input("input.txt")));
 }
