9f01492c94d02ee7ce6a69bb5f5e3a389837ee53
Stephen Hines <srhines@gmail.com>
2024-12-27 18:01:56 -0500

    More code to try out adder checking

--- aoc24/src/main.rs
+++ aoc24/src/main.rs
@@ -14,7 +14,7 @@ fn get_input(filename: &str) -> Vec<String> {
     lines
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum GateOp {
     And,
     Or,
@@ -120,6 +120,46 @@ impl Circuit {
         self.wires.insert(wire.name.clone(), wire.clone());
     }
 
+    fn resolve_wire_no_mut(&self, name: &str) -> WireState {
+        let wire = self.wires.get(name).unwrap().clone();
+        let state = wire.state;
+        match state {
+            WireState::True | WireState::False => state,
+            WireState::Gated => {
+                let gate = wire.gate.as_ref().unwrap();
+                let operand_1 = self.resolve_wire_no_mut(&gate.operand_1);
+                assert!(operand_1 == WireState::True || operand_1 == WireState::False);
+                let operand_2 = self.resolve_wire_no_mut(&gate.operand_2);
+                assert!(operand_2 == WireState::True || operand_2 == WireState::False);
+                let gate_op = &gate.gate_op;
+                let result = match gate_op {
+                    GateOp::And => {
+                        if operand_1 == WireState::True && operand_2 == WireState::True {
+                            WireState::True
+                        } else {
+                            WireState::False
+                        }
+                    }
+                    GateOp::Or => {
+                        if operand_1 == WireState::True || operand_2 == WireState::True {
+                            WireState::True
+                        } else {
+                            WireState::False
+                        }
+                    }
+                    GateOp::Xor => {
+                        if operand_1 != operand_2 {
+                            WireState::True
+                        } else {
+                            WireState::False
+                        }
+                    }
+                };
+                result
+            }
+        }
+    }
+
     fn resolve_wire(&mut self, name: &str) -> WireState {
         let wire = self.wires.get(name).unwrap().clone();
         let state = wire.state;
@@ -194,31 +234,119 @@ impl Circuit {
             self.resolve_wire(&name);
         }
 
-        /*let mut zvec = self
-            .wires
-            .keys()
-            .filter(|&n| n.starts_with('z'))
-            .collect::<Vec<_>>();
-        zvec.sort();
-        zvec.reverse();
-        let mut result = 0;
-        for &name in zvec.iter() {
-            result <<= 1;
-            if self.wires.get(name).unwrap().state == WireState::True {
-                result += 1;
-            }
-        }*/
+        //dbg!(&self);
         let result = self.get_value_from_bit_vector("z");
+        println!("Result: {result}");
+        result
+    }
+
+    fn simulate_no_mut(&self) -> u64 {
+        let mut names = Vec::new();
+        for (name, _) in self.wires.iter() {
+            names.push(name.clone());
+        }
+        let mut result = 0;
+
+        for (name, _) in self.wires.iter() {
+            if name.starts_with('z') {
+                let num = name[1..].parse::<u32>().unwrap();
+                let bit_result = self.resolve_wire_no_mut(name);
+                if bit_result == WireState::True {
+                    result |= 1 << num;
+                }
+            }
+        }
 
         //dbg!(&self);
         println!("Result: {result}");
+        result
+    }
+
+    // Returns true if invalid found
+    fn invalid_xy_xor(&self, num: u32, wire: &String) -> bool {
+        match &self.wires.get(wire).unwrap().gate {
+            None => {
+                return true;
+            }
+            Some(vz_zz) => {
+                let gate = &vz_zz.gate_op;
+                if *gate != GateOp::Xor {
+                    return true;
+                }
+                let x_zz = format!("x{:02}", num);
+                let y_zz = format!("y{:02}", num);
+                let lhs = &vz_zz.operand_1;
+                let rhs = &vz_zz.operand_2;
+                if (*lhs == x_zz && *rhs == y_zz) || (*lhs == y_zz && *rhs == x_zz) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    // Returns true if invalid found
+    fn check_zzz_xor(&self, num: u32, invalid: &mut Vec<String>) -> bool {
+        let z_zz = format!("z{:02}", num);
+        if num == 0 {
+            // We can check this later, but it's different without a carry
+            let res = self.invalid_xy_xor(num, &z_zz);
+            if res {
+                invalid.push(z_zz);
+            }
+            return !res;
+        }
+        match &self.wires.get(&z_zz).unwrap().gate {
+            None => {
+                invalid.push(z_zz);
+                return true;
+            },
+            Some(vz_zz) => {
+                let gate = &vz_zz.gate_op;
+                if *gate != GateOp::Xor {
+                    invalid.push(z_zz);
+                    return true;
+                }
+                let x_zz = format!("x{:02}", num);
+                let y_zz = format!("y{:02}", num);
+                let lhs = &vz_zz.operand_1;
+                let rhs = &vz_zz.operand_2;
+                let invalid_xy_lhs = self.invalid_xy_xor(num, lhs);
+                let invalid_xy_rhs = self.invalid_xy_xor(num, rhs);
+                if invalid_xy_lhs && invalid_xy_rhs {
+                    invalid.push(rhs.clone());
+                    return true;
+                }
+
+                /*if !(*lhs == x_zz && *rhs == y_zz) || (*lhs == y_zz && *rhs == x_zz) {
+                    invalid.push(z_zz);
+                }*/
+            },
+        }
+        false
+    }
+
+    fn find_swaps(&self) {
+        //dbg!(&self);
+        let z = self.simulate_no_mut();
+        //let result = self.get_value_from_bit_vector("z");
+        //println!("Result: {result}");
         let x = self.get_value_from_bit_vector("x");
         let y = self.get_value_from_bit_vector("y");
         println!("x: {x:46b}");
         println!("y: {y:46b}");
-        println!("z: {result:46b}");
+        println!("z: {z:46b}");
         println!("c: {:46b}", x + y);
-        result
+
+
+        let mut invalid: Vec<String> = Vec::new();
+        for i in 0..46 {
+            if self.check_zzz_xor(i, &mut invalid) {
+                //let z_xx = format!("z{:02}", i);
+                //invalid.push(z_xx);
+            }
+        }
+        dbg!(invalid);
     }
 }
 
@@ -256,4 +384,5 @@ fn main() {
     read_circuit(&get_input("prelim.txt")).simulate();
     read_circuit(&get_input("prelim2.txt")).simulate();
     read_circuit(&get_input("input.txt")).simulate();
+    read_circuit(&get_input("input.txt")).find_swaps();
 }
