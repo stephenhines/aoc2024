7f4ee379e726adc402f29315633a28073ce236bf
Stephen Hines <srhines@gmail.com>
2025-01-07 22:25:06 -0800

    Problem 5 part 2 for AOC2024

--- aoc05/src/main.rs
+++ aoc05/src/main.rs
@@ -23,10 +23,15 @@ enum Precedence {
 // Indexed as [BEFORE * 100 + AFTER]
 type PrecedenceTable = [Precedence; 100 * 100];
 
-fn calculate_middle_page_sum(lines: &[String]) -> usize {
-    //let mut precedence_table: PrecedenceTable = [[Precedence::Unknown; 100]; 100];
+struct PageData {
+    precedence_table: PrecedenceTable,
+    page_list: Vec<Vec<usize>>,
+}
+
+fn read_page_info(lines: &[String]) -> PageData {
     let mut precedence_table: PrecedenceTable = [Precedence::Unknown; 100 * 100];
-    let mut sum = 0;
+    let mut page_list = Vec::new();
+
     let mut line_iter = lines.iter();
     loop {
         match line_iter.next() {
@@ -53,58 +58,120 @@ fn calculate_middle_page_sum(lines: &[String]) -> usize {
         let mut page_numbers = Vec::new();
         let toks = line.split(',').collect::<Vec<_>>();
         for tok in toks {
-            let val = tok.parse::<usize>().unwrap();
-            page_numbers.push(val);
+            page_numbers.push(tok.parse::<usize>().unwrap());
+        }
+        page_list.push(page_numbers);
+    }
+
+    PageData {
+        precedence_table,
+        page_list,
+    }
+}
+
+fn check_valid(precedence_table: &PrecedenceTable, page_numbers: &[usize]) -> bool {
+    let mut seen_pages = Vec::new();
+    for page in page_numbers {
+        if seen_pages.contains(page) {
+            panic!("Duplicate page {page}");
         }
 
-        let mut valid = true;
-        let mut seen_pages = Vec::new();
-        for page in page_numbers {
-            if seen_pages.contains(&page) {
-                panic!("Duplicate page {page}");
-            }
-
-            for before in &seen_pages {
-                if precedence_table[before * 100 + page] == Precedence::After {
-                    valid = false;
-                    break;
-                };
-            }
-
-            if !valid {
-                break;
-            }
-
-            seen_pages.push(page);
+        for before in &seen_pages {
+            if precedence_table[before * 100 + page] == Precedence::After {
+                return false;
+            };
         }
 
-        if !valid {
-            continue;
-        }
+        seen_pages.push(*page);
+    }
 
-        //println!("valid line: {line}");
-        let mid = seen_pages.len() / 2;
-        sum += seen_pages[mid];
+    true
+}
+
+fn calculate_middle_page_sum(page_data: &PageData) -> usize {
+    let mut sum = 0;
+    let page_list = &page_data.page_list;
+    let precedence_table = &page_data.precedence_table;
+    for page_numbers in page_list {
+        if check_valid(precedence_table, page_numbers) {
+            let mid = page_numbers.len() / 2;
+            sum += page_numbers[mid];
+        }
     }
 
     println!("Total middle page sum: {sum}");
     sum
 }
 
+fn calculate_invalid_middle_page_sum(page_data: &PageData) -> usize {
+    let mut sum = 0;
+    let page_list = page_data.page_list.clone();
+    let precedence_table = &page_data.precedence_table;
+    for mut page_numbers in page_list {
+        // Only consider invalid sequences
+        if check_valid(precedence_table, &page_numbers) {
+            continue;
+        }
+        for b in 0..page_numbers.len() {
+            for a in b + 1..page_numbers.len() {
+                let before = page_numbers[b];
+                let after = page_numbers[a];
+                // Simple swap if we're supposed to actually be after
+                if precedence_table[before * 100 + after] == Precedence::After {
+                    page_numbers[b] = after;
+                    page_numbers[a] = before;
+                }
+            }
+        }
+
+        let valid = check_valid(precedence_table, &page_numbers);
+        if valid {
+            //println!("valid: {:?}", page_numbers);
+            let mid = page_numbers.len() / 2;
+            sum += page_numbers[mid];
+        } else {
+            panic!("Invalid swapped sequence {:?}", page_numbers);
+        }
+    }
+
+    println!("Total invalid middle page sum: {sum}");
+    sum
+}
+
 #[test]
 fn test_prelim() {
-    let sum = calculate_middle_page_sum(&get_input("prelim.txt"));
+    let page_data = read_page_info(&get_input("prelim.txt"));
+    let sum = calculate_middle_page_sum(&page_data);
     assert_eq!(sum, 143);
 }
 
 #[test]
 fn test_part1() {
-    let sum = calculate_middle_page_sum(&get_input("input.txt"));
+    let page_data = read_page_info(&get_input("input.txt"));
+    let sum = calculate_middle_page_sum(&page_data);
     assert_eq!(sum, 4578);
 }
 
+#[test]
+fn test_prelim2() {
+    let page_data = read_page_info(&get_input("prelim.txt"));
+    let sum = calculate_invalid_middle_page_sum(&page_data);
+    assert_eq!(sum, 123);
+}
+
+#[test]
+fn test_part2() {
+    let page_data = read_page_info(&get_input("input.txt"));
+    let sum = calculate_invalid_middle_page_sum(&page_data);
+    assert_eq!(sum, 6179);
+}
 
 fn main() {
-    calculate_middle_page_sum(&get_input("prelim.txt"));
-    calculate_middle_page_sum(&get_input("input.txt"));
+    let page_data = read_page_info(&get_input("prelim.txt"));
+    calculate_middle_page_sum(&page_data);
+    calculate_invalid_middle_page_sum(&page_data);
+
+    let page_data = read_page_info(&get_input("input.txt"));
+    calculate_middle_page_sum(&page_data);
+    calculate_invalid_middle_page_sum(&page_data);
 }
